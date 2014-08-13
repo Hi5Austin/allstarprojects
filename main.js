a5e542c1aff21468b52eac86939bbb638064cfa3
Austin Carvey <austin@carvey.me>
2014-08-13 16:49:53 -0400

    fff

--- main.js
+++ main.js
@@ -142,7 +142,6 @@ allComs.on('child_added',function(snapshot) {
         if(stuff[key].link === projects_url) {
           $("#dez").append('<a>' + stuff[key].name + '</a><br>');
           $("#dez").append('<a>' + stuff[key].des + '</a><br>');
-          $("#dez").append('<a> A '+ stuff[key].type + ' by '+ stuff[key].creator + '</a><br>');
           //$("#indie").append('<input id="com" placeholder="Leave a comment"><input id="addc" type="Submit">');
           $("#dez").append('<input id="comname" placeholder="Name"><input id="com" placeholder="Leave a comment"><input id="addc" type="Submit">');
           //$("#indie").append('<div id="fb-root"></div>');
